1f4f3f00436dec57397a95ef4b6861ca54c11fde
Tim Condon <0xTim@users.noreply.github.com>
2020-12-16 16:18:39 +0000

    Use my gitless forks

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/0xTim/fluent-kit", from: "1.0.0"),
+        .package(url: "https://github.com/0xTim/vapor", from: "4.0.0"),
     ],
     targets: [
         .target(name: "Fluent", dependencies: [
